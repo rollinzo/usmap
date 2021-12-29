5ec43af4fa76d23303d038ade6f5d099ca8cab0c
Kevin D. Rollins <weblog@kevinrollins.com>
2021-12-28 19:02:58 -0500

    added mouseover and click handling

--- src/Map.js
+++ src/Map.js
@@ -1,38 +1,57 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {usMap} from './coords'
 import './Map.css';
 import {svgParse2Resize} from './svgParse2Resize';
 
 export default function Map(){
 
+  const [selectedState, setSelectedState] = useState("Click on a State");
 
-  const filterCommas = (stateData) => {
-    return stateData.replace(',',' ');
+  const announcedState = () =>{
+    if (selectedState === "Click on a State") {
+      return selectedState;
+    } else {
+      return "You clicked on :" + selectedState;
+    }
   }
 
-  const State = (props) => {
-    return <>
-              <path d={svgParse2Resize(filterCommas(usMap[props.state]),0.4)} fill="lightgreen" stroke="black" />
-           </>;
-  }
   const allStates = () => {
     return Object.keys(usMap).map((myState) => {
           // if (myState != "ak" && myState != "wa") {
-             return <State key={myState} state={myState} />
+             return <State key={myState} state={myState} fill="lightblue" stroke="black" announceMe={setSelectedState}/>
            //}
            // return <div>myState</div>
     });
   }
-  useEffect(()=>{
-    console.log(svgParse2Resize("M 200 200 L 350 200 L 500 350 L 500 500 L 350 650 L 200 650 L 50 500 L 50 350  Z",1.0));
-  },[]);
-  const someStates = () => {
-    return [<State state="tx" />, <State state="va" />, <State state="nc" />];
-  }
+  // useEffect(()=>{
+  //   console.log(svgParse2Resize("M 200 200 L 350 200 L 500 350 L 500 500 L 350 650 L 200 650 L 50 500 L 50 350  Z",1.0));
+  // },[]);
+  // const someStates = () => {
+  //   return [<State state="tx" />, <State state="va" />, <State state="nc" />];
+  // }
 
   return <div id="mapDiv">
-    <svg id="theMap" width="500" height="500">
+    <svg id="theMap" width="500" height="300">
      {allStates()}
     </svg>
+    <h1>{announcedState()}</h1>
   </div>;
+}
+
+const filterCommas = (stateData) => {
+  return stateData.replace(',',' ');
+}
+
+const State = (props) => {
+  const [myFill, setMyFill] = useState(props.fill);
+  const [myStroke, setMyStroke] = useState(props.stroke);
+  const highlightState = () => {
+    setMyFill("red");
+  }
+  const deHighlightState = () => {
+    setMyFill("lightblue");
+  }
+  return <>
+            <path d={svgParse2Resize(filterCommas(usMap[props.state]),0.4)} fill={myFill} stroke={myStroke} onMouseOver={highlightState} onMouseOut={deHighlightState} onClick={()=>{ props.announceMe(props.state)}}/>
+         </>;
 }
